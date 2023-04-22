348e91eda3f35d979cdc6771e908810196316e42
ZoSand <valentin.lambert.ba@gmail.com>
2023-04-22 17:19:14 +0200

    moved to Core directory

--- premake5.lua
+++ premake5.lua
@@ -1,44 +1,6 @@
 include "BerylliumConfig.lua"
 
 group "Engine"
-	project "Beryllium"
-		location "" 
-		kind "SharedLib"
-		language "C++"
-		cppdialect "C++17"
-		systemversion "latest"
-		files {
-			"**.cd",
-			"include/**.hpp",
-			"include/**.inl",
-			"src/**.cpp"
-		}
-		defines {
-			"BE_EXPORTS"
-		}
-		includedirs {
-			"include"
-		}
-
-		filter { "system:linux" }
-			removefiles { 
-				"**/Platforms/Linux/**",
-				"**/Platforms/MacOSX/**"
-			}
-
-		filter { "system:macosx" }
-			removefiles { 
-				"**/Platforms/Linux/**",
-				"**/Platforms/Windows/**"
-			}
-
-		filter { "system:windows" }
-			defines {
-				"BE_BUILD_DLL"
-			}
-			removefiles {
-				"**/Platforms/Linux/**",
-				"**/Platforms/MacOSX/**"
-			}	
+	include "Core"
 
 group "Game"
